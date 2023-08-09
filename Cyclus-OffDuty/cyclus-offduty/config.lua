8a27060dba539ce6bbb9be19f23840a38916e34f
Tiziukas <141794006+Tiziukas@users.noreply.github.com>
2023-08-09 02:49:46 +0100

    Update config.lua
    Update config.lua for logs.

--- Cyclus-OffDuty/cyclus-offduty/config.lua
+++ Cyclus-OffDuty/cyclus-offduty/config.lua
@@ -4,8 +4,8 @@ Cyclus.OffDuty = {
     {
         job = 'police', -- Job name (Database)
         offjob = 'offpolice', -- Off-Job name (Database)
-        coords = vector3(441.8991, -982.3894, 30.7424), -- Coords voor de drawmarker & drawtext
-        text = '[~b~E~w~] Interactie', -- Text die word gedisplayed(Drawtext)
+        coords = vector3(443.2195, -981.4097, 30.6895), -- Coords voor de drawmarker & drawtext
+        text = 'Spausk [~b~E~w~]', -- Text die word gedisplayed(Drawtext)
         distancetext = 1, -- Distance, om de interactie te kunnen gebruiken (Drawtext)
         distancemarker = 4, -- Distance, om de marker te kunnen zien (Drawmarker)
         img = "https://cdn.discordapp.com/attachments/985133441651732490/988015833185910834/politie.png" -- Link voor de image die word gedisplayed
@@ -13,8 +13,8 @@ Cyclus.OffDuty = {
     {
         job = 'ambulance', -- Job name (Database)
         offjob = 'offambulance', -- Off-Job name (Database)
-        coords = vector3(339.2108, -589.6216, 43.2841), -- Coords voor de drawmarker & drawtext
-        text = '[~b~E~w~] Interactie', -- Text die word gedisplayed(Drawtext)
+        coords = vector3(359.8109, -1417.9910, 32.5093), -- Coords voor de drawmarker & drawtext
+        text = 'Spausk [~b~E~w~]', -- Text die word gedisplayed(Drawtext)
         distancetext = 1, -- Distance, om de interactie te kunnen gebruiken (Drawtext)
         distancemarker = 4, -- Distance, om de marker te kunnen zien (Drawmarker)
         img = "https://cdn.discordapp.com/attachments/985133441651732490/988018868968624198/veiligheidsregio.png" -- Link voor de image die word gedisplayed
@@ -22,8 +22,8 @@ Cyclus.OffDuty = {
     {
         job = 'mechanic', -- Job name (Database)
         offjob = 'offmechanic', -- Off-Job name (Database)
-        coords = vector3(805.7999, -885.3664, 25.2508), -- Coords voor de drawmarker & drawtext
-        text = '[~b~E~w~] Interactie', -- Text die word gedisplayed(Drawtext)
+        coords = vector3(-329.8318, -155.7067, 44.5738), -- Coords voor de drawmarker & drawtext
+        text = 'Spausk [~b~E~w~]', -- Text die word gedisplayed(Drawtext)
         distancetext = 1, -- Distance, om de interactie te kunnen gebruiken (Drawtext)
         distancemarker = 4, -- Distance, om de marker te kunnen zien (Drawmarker)
         img = "https://cdn.discordapp.com/attachments/985133441651732490/988017452547997766/anwb.png" -- Link voor de image die word gedisplayed
@@ -31,8 +31,8 @@ Cyclus.OffDuty = {
     {
         job = 'firefighter', -- Job name (Database)
         offjob = 'offfirefighter', -- Off-Job name (Database)
-        coords = vector3(197.9642, -1647.3796, 29.8032), -- Coords voor de drawmarker & drawtext
-        text = '[~b~E~w~] Interactie', -- Text die word gedisplayed(Drawtext)
+        coords = vector3(-139.0194, 6295.5898, 35.6795), -- Coords voor de drawmarker & drawtext
+        text = 'Spausk [~b~E~w~]', -- Text die word gedisplayed(Drawtext)
         distancetext = 1, -- Distance, om de interactie te kunnen gebruiken (Drawtext)
         distancemarker = 4, -- Distance, om de marker te kunnen zien (Drawmarker)
         img = "https://cdn.discordapp.com/attachments/985133441651732490/988018059925139466/brandweer.png" -- Link voor de image die word gedisplayed
@@ -41,7 +41,11 @@ Cyclus.OffDuty = {
 
 Cyclus.Function = {
     {
-        NotifyTitle = 'Operationeel Centrum',
-        NotifyText = 'Werk status geupdate!',
+        NotifyTitle = 'Centras',
+        NotifyText = 'Tu isejiai on/off duty!',
     }
 }
+
+Cyclus.police = ""
+Cyclus.ambulance = ""
+Cyclus.mechanic = ""
